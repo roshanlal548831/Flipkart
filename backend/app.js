3f2d0514c8b439e51882d5fbf1d7660f9443b190
roshanlal548831 <roshanlal160405@gmail.com>
2024-07-15 13:14:57 +0530

    git add  mistake

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const userRouter = require("./router/userRoute")
+const userRouter = require("./routers/userRoute")
 
 
 app.use("/api",userRouter)
